1052f1880673c5e71814eb86c3fa6f60116de64d
AswathyRamprabhu <aswathysivarajan217@gmail.com>
2023-11-08 19:05:59 +0530

    eighth commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -31,7 +31,7 @@ userRoute.post('/verifyotp', userController.userSignedup);
 userRoute.post('/signup', userController.userSignedup);
 
 // User forgot password routes
-userRoute.get('/forgotpassword',  auth. setCacheControl, userController.userForgotPassword);
+userRoute.get('/forgotpassword', userController.userForgotPassword);
 userRoute.post('/resetpassword', auth. setCacheControl, auth.checkSession, userController.userResetPassword);
 
 // User home route
